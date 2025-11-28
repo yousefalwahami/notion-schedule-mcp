259570bc65e2a0f600704d6d296a2528674b51ac
Yousef Al-Wahami <alwahami.yousef@gmail.com>
2025-11-27 21:14:40 -0500

    feat: add clear trash button

--- app/page.tsx
+++ app/page.tsx
@@ -685,10 +685,27 @@ export default function Home() {
         {/* Parsed Results */}
         {parsedResults.length > 0 && (
           <div className="mt-8">
-            <h2 className="text-2xl font-bold mb-6 text-zinc-900 dark:text-zinc-50">
-              Extracted Assignments
-            </h2>
-
+            <div className="flex justify-between items-center">
+              <h2 className="text-2xl font-bold mb-6 text-zinc-900 dark:text-zinc-50">
+                Extracted Assignments
+              </h2>
+            
+              <button
+                onClick={() => {
+                  sessionStorage.removeItem("parsedResults");
+                  setParsedResults([]);
+                }}
+                className="mr-6 mb-6 text-zinc-400 hover:text-red-500 dark:hover:text-red-500 cursor-pointer"
+              >
+                <svg className="" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M3 6h18"/>
+                  <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
+                  <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"/>
+                  <path d="M10 11v6"/>
+                  <path d="M14 11v6"/>
+                </svg>
+              </button>
+            </div>
             {parsedResults.map((syllabus, idx) => (
               <div
                 key={idx}
